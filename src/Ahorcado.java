b0af3081a8f95cc9dd028bf07e6a48ef685bf88f
Ruben <rubenrdc2003@gmail.com>
2024-05-28 01:52:40 -0300

    practica Ahorcados en Java

---
+++ src/Ahorcado.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ahorcado {
+    public static void main(String[] args) throws Exception {
+        //String[] PalabrasSecretas= new String[10];
+        String[] PalabrasSecretas = {"sube","avion","camionero","electrista"};
+        int intentosMaximos = 10,errores = 0;
+        boolean gano=false;
+        Random r = new Random();
+        Scanner scan = new Scanner(System.in);
+        int numeroAleatorio = r.nextInt(PalabrasSecretas.length);
+        
+        String PalabraSecretaSeleccionada = PalabrasSecretas[numeroAleatorio];
+        System.out.println(numeroAleatorio);
+        
+        char caracteresRecibidos[] = new char[PalabraSecretaSeleccionada.length()];
+        for(int i=0;i<PalabraSecretaSeleccionada.length();i++){
+            caracteresRecibidos[i]='_';
+        }
+        
+        System.out.println("Bienvenidos a el ahorcados de Ruben!");
+        while (!gano && errores<intentosMaximos) {
+            
+            System.out.println("La palabra por adivinar es : "+ String.valueOf(caracteresRecibidos) + " y contiene "+ PalabraSecretaSeleccionada.length()+" Caracteres");
+            System.out.print("Ingrese una letra:");
+            char letra = Character.toLowerCase(scan.next().charAt(0));
+            boolean contieneCaracter=false;
+
+            for(int i=0;i<PalabraSecretaSeleccionada.length();i++){
+                if (PalabraSecretaSeleccionada.charAt(i)==letra) {
+                    caracteresRecibidos[i] = letra;
+                    contieneCaracter=true;
+                }
+            }
+            if (!contieneCaracter) {
+                errores++;
+                System.out.println("La letra ingresada no se encuentra en la palabra por adivinar, intentos restantes: "+(intentosMaximos-errores));
+                if (intentosMaximos==errores) {
+                    System.out.println("Haz Perdido el juego.");
+                }
+            }else if (String.valueOf(caracteresRecibidos).equals(PalabraSecretaSeleccionada)) {
+                System.out.println("Felicidades! Haz Ganado! La la palabra ganadora era: "+PalabraSecretaSeleccionada);
+                gano=true;
+            }
+        }
+        scan.close();
+    }
+}
